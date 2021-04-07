bad6e0f93f1e6ddc550bde939d81b3dc9aeee3fe
lidzhiev <lidzhiev0912@gmail.com>
2021-04-07 19:18:34 +0300

    myapp

---
+++ app/src/main/java/com/e/apptrainingchildren/arrays/Color.java
@@ -0,0 +1,9 @@
+package com.e.apptrainingchildren.arrays;
+
+import com.e.apptrainingchildren.R;
+
+public class Color {
+    public String[] arrColor = new String[]{"Красный", "Синий", "Зеленый", "Розовый"};
+    public int[] arrColorDrawable = new int[]{R.color.red, R.color.blue,R.color.green, R.color.pink,};
+    public int[] arrColorSound = new int[]{R.raw.yellow, R.raw.yellow,R.raw.yellow,R.raw.yellow,};
+}
